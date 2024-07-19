e0d4e081b954b481a0d47c4a17b0951811354565
unknown <philippeyams@gmail.com>
2024-07-19 10:57:21 +0200

    Fichier dataScience

--- DataScience.py
+++ DataScience.py
@@ -7,7 +7,21 @@ Created on Thu Jul 11 10:18:51 2024
 
 import pandas as pd
 import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
 
-data=""
+# Load the NetCDF4 file
+data = "data.nc4"
+dat = xr.open_dataset(data)
 
-dat=pd.read_csv(data)
+
+
+help(xr.open_dataset)
+
+
+# Print the dataset structure to check dimensions
+print(dat)
+
+# Access the precipitation variable
+precipitation = dat['precipitation']
+print(precipitation)
